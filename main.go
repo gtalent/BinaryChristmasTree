e7066d08f70aa4cba74a8f5a16758332e3e9a341
Gary Talent <gtalent2@gmail.com>
2011-11-13 01:48:47 -0600

    Initial import.

---
+++ main.go
@@ -0,0 +1,96 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+type color struct {
+	r, g, b byte
+}
+
+func (me *color) toUint32() uint32 {
+	return (uint32(me.r) << 16) | (uint32(me.g) << 8) | uint32(me.b)
+}
+
+func encode(val uint, bits int) string {
+	str := ""
+	for i := 0; i < bits; i++ {
+		r := val & uint(math.Pow(2, float64(i)))
+		if r == 0 {
+			str += "0"
+		} else {
+			str += "1"
+		}
+	}
+	return str
+}
+
+type node struct {
+	left  uint8
+	color uint32
+	right uint8
+}
+
+func (me *node) toBinary() string {
+	str := encode(uint(me.left), 8)
+	str += encode(uint(me.color), 32)
+	str += encode(uint(me.right), 8)
+	return str
+}
+
+func main() {
+	tree := make([]node, 1)
+	var colors []color
+	colors = append(colors, color{255, 0, 0})
+	colors = append(colors, color{0, 255, 0})
+	colors = append(colors, color{0, 0, 255})
+	colors = append(colors, color{255, 255, 0})
+	colors = append(colors, color{0, 255, 255})
+	colors = append(colors, color{255, 0, 255})
+	colors = append(colors, color{255, 255, 255})
+
+	for i := 0; i < len(colors); i++ {
+		tree = addNode(0, tree, colors[i])
+	}
+
+	for i := 1; i < len(tree); i++ {
+		fmt.Print(tree[i].toBinary())
+	}
+	fmt.Println()
+}
+
+func traverse(tree []node, pt uint8) {
+	if tree[pt].left != 0 {
+		traverse(tree, tree[pt].left)
+	}
+	fmt.Println("\t", tree[pt].color)
+	if tree[pt].right != 0 {
+		traverse(tree, tree[pt].right)
+	}
+}
+
+func addNode(nodePt uint8, tree []node, color color) ([]node) {
+	if tree[nodePt].color > color.toUint32() {
+		if tree[nodePt].left != 0 {
+			tree = addNode(tree[nodePt].left, tree, color)
+		} else {
+			var node node
+			node.color = color.toUint32()
+			tree[nodePt].left = uint8(len(tree))
+			tree = append(tree, node)
+		}
+		return tree
+	} else {
+		if tree[nodePt].right != 0 {
+			tree = addNode(tree[nodePt].right, tree, color)
+		} else {
+			var node node
+			node.color = color.toUint32()
+			tree[nodePt].right = uint8(len(tree))
+			tree = append(tree, node)
+		}
+		return tree
+	}
+	return nil
+}
